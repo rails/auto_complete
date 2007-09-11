7b18f05af68dec0f9e45b1a01d6b7c94e306caeb
David Heinemeier Hansson <david@loudthinking.com>
2007-09-11 15:01:16 +0000

    Fix auto complete loading (closes #9528) [josh]

--- init.rb
+++ init.rb
@@ -1,2 +1,2 @@
-ActionController::Base.send :include, AutoComplete 
-ActionView::Helpers.send :include, AutoCompleteMacrosHelper
+ActionController::Base.send :include, AutoComplete
+ActionController::Base.helper AutoCompleteMacrosHelper
